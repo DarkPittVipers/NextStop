925e03ba77063e6a4dbed3153c800b38753904fe
mhhutton <mhhutton@gmail.com>
2021-07-12 13:28:39 -0600

    updates components

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,25 +1,97 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import axios from 'axios';
+import { Grid, Typography, InputBase, Button } from '@material-ui/core';
+
+// MATERIAL UI ICONS
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
 import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   nav: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     fontFamily: '"Oswald", sans-serif',
     backgroundColor: '#1A535C',
     boxShadow: '0 20px 20px 20px rgba(0, 0, 0, 0.15), 0 5px 15px 15px rgba(0, 0, 0, 0.15)',
     padding: '10px 30px',
     margin: 0,
   },
+  searchBar: {
+    border: '2px solid white',
+    borderRadius: '8px',
+  },
+  destSearch: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    color: 'white',
+  },
+  destFont: {
+    fontFamily: '"Oswald", sans-serif',
+    fontWeight: '500',
+    fontSize: '20px',
+  },
+  loginCont: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+    color: 'white',
+  },
+  loginBtn: {
+    color: 'white',
+    border: '2px solid white',
+    borderRadius: '8px',
+  },
 }));
 
 export default function Navigation() {
   const classes = useStyles();
+
+  const login = () => {
+    axios.get('/login');
+  };
+
   return (
     <Grid
       container
       className={classes.nav}
     >
       <img src="assets/NextStopLogo.svg" height="64" alt="logo" />
+      <Grid
+        className={classes.destSearch}
+        item
+      >
+        <Typography className={classes.destFont}>Destination: &nbsp; &nbsp;</Typography>
+        <div className={classes.searchBar}>
+          <SearchIcon />
+          <InputBase
+            className={classes.inputBox}
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </div>
+      </Grid>
+
+      <Grid
+        className={classes.loginCont}
+        item
+      >
+        <AccountCircle />
+        <Button
+          className={classes.loginBtn}
+          onClick={() => { login(); }}
+        >
+        LOG IN
+        </Button>
+      </Grid>
+
     </Grid>
   );
 }

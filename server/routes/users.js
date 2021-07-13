53d612ad500000a97c15f460ecfffeaa4ce6022a
mhhutton <mhhutton@gmail.com>
2021-07-13 16:45:20 -0600

    uncomments html

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+// const UserProfile = require('../../client/src/components/UserProfile/UserProfile.jsx');
 const secured = require('../lib/middleware/secured');
 
 const router = express.Router();
@@ -6,13 +7,13 @@ const router = express.Router();
 router.get('/profile', secured(), (req, res) => {
   // TODO display user profile page
   res.status(200).send(
-    // `<!DOCTYPE html>
-    // <html>
-    //   <body>
-    //     Profile Page
-    //     <a href="/">Home</a>
-    //   </body>
-    // </html>`,
+    `<!DOCTYPE html>
+    <html>
+      <body>
+        Profile Page
+        <a href="/">Home</a>
+      </body>
+    </html>`,
   );
 });
 

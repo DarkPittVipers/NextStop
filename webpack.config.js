e0c243ce4e6baf505cdfe65b2315898e7682f2bf
mhhutton <mhhutton@gmail.com>
2021-07-14 15:19:04 -0600

    adds commas

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,13 @@ const SRC_DIR = path.join(__dirname, '/client/src');
 const DIST_DIR = path.join(__dirname, '/client/dist');
 
 module.exports = {
+  devServer: {
+    historyApiFallback: true,
+  },
   entry: `${SRC_DIR}/index.jsx`,
   output: {
     filename: 'bundle.js',
-    path: DIST_DIR
+    path: DIST_DIR,
   },
   module: {
     rules: [
@@ -15,9 +18,9 @@ module.exports = {
         test: /\.(js|jsx)?/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
-        }
-      }
-    ]
-  }
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
 };

b6e0896b25131cc4376b5f5695e423fd37560523
Pix3lAssassin <th3koolperson@gmail.com>
2021-07-15 14:44:09 -0600

    added routes for meta data

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const home = require('./home');
 const users = require('./users');
 const auth = require('./auth');
 const flights = require('./flights');
+const meta = require('./meta');
 
 module.exports = {
   events,
@@ -12,5 +13,6 @@ module.exports = {
   users,
   auth,
   flights,
+  meta,
 };
 

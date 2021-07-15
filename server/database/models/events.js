83433a42a6cd69113b297f4555697bc3284f290c
Pix3lAssassin <th3koolperson@gmail.com>
2021-07-15 14:54:42 -0600

    updated event schema

--- server/database/models/events.js
+++ server/database/models/events.js
@@ -2,6 +2,21 @@ const mongoose = require('mongoose');
 
 const EventSchema = mongoose.Schema({
   userId: String,
+  type: String,
+  subType: String,
+  id: String,
+  self: {
+      href: String,
+      methods: [String],
+  },
+  geoCode: {
+      latitude: Number,
+      longitude: Number,
+  },
+  name: String,
+  category: String,
+  rank: Number,
+  tags: [String],
 });
 
 const Event = mongoose.model('event', EventSchema);
